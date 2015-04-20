5c712e81d7f2c07c64976d0e37fce9b262147126
airyland <airyland@qq.com>
2015-04-20 17:54:17 +0800

    add baidu api

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 var restler = require('restler');
+var config = require('./config.json');
 
-exports.query = function(ip, cb) {
-	restler.get("http://ip.taobao.com/service/getIpInfo.php?ip=" + ip).on('complete', function(data) {
-		cb(JSON.parse(data).data);
-	});
+// baidu http://developer.baidu.com/map/index.php?title=webapi/ip-api
+exports.query = function(ip, cb, provider) {
+	if (provider === 'taobao') {
+		restler.get("http://ip.taobao.com/service/getIpInfo.php?ip=" + ip).on('complete', function(data) {
+			var address = JSON.parse(data).data;
+			cb({
+				country: address.country_id,
+				province: address.region.replace('省', ''),
+				city: address.city.replace('市', '')
+			});
+		});
+	} else {
+		restler.get("http://api.map.baidu.com/location/ip?ak=" + config.baidu.ak + "&ip=" + ip).on('complete', function(data) {
+			data = data.address.split('|');
+			cb({
+				country: data[0],
+				province: data[1],
+				city: data[2]
+			});
+		});
+	}
 };
--- query.js
+++ query.js
@@ -1,4 +1,8 @@
 var search = require('./index');
 search.query('183.6.130.250',function(data){
-	console.log(data);
+	console.log('from taobao',data);
+},'taobao');
+
+search.query('183.6.130.250',function(data){
+	console.log('from baidu',data);
 });
